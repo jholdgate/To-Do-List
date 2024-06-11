26fa664e6bbda1b01c431901d04327545ff7b116
jholdgate <jholdgate1@yahoo.com>
2024-06-11 16:16:32 -0400

    It Works

---
+++ index.js
@@ -0,0 +1,36 @@
+console.log('My code is running');
+
+
+// document.getElementById("complete").addEventListener("click", function() {
+//     let listItem = document.getElementById("list")
+//     listItem.style.textDecoration = "line-through";
+// })
+
+
+function addTask() {
+  var newTask = document.getElementById("createTask").value;
+
+  if (!newTask.trim()) {
+    alert("Please enter a task.");
+    return
+  }
+  let newTaskItem = document.createElement("li");
+    newTaskItem.textContent = newTask;
+    newTaskItem.addEventListener("click", function() {
+        newTaskItem.style.textDecoration = "line-through";
+    });
+  let checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.addEventListener("change", function (){
+        if (this.checked) {
+        newTaskItem.parentNode.removeChild(newTaskItem);
+        }
+});
+  newTaskItem.appendChild(checkbox);
+
+  document.getElementById("list").appendChild(newTaskItem);
+
+  document.getElementById("createTask").value = "";
+}
+
+
